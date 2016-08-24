ee3292b6e904ceb9ca45981a323cd24d351e91bc
Santiago G. Marín <santiago@finaldevstudio.com>
2016-08-24 19:18:07 -0300

    Compiled distributables

---
+++ dist/ng-pagination.js
@@ -0,0 +1,59 @@
+(function(window) {
+  "use strict";
+  var ng = window.angular;
+  function ngPaginationDirectiveFn($http) {
+    function ngPaginationDirectiveLinkFn($scope, $element, $attrs) {
+      function onCountSuccess(res) {
+        $scope.total = parseInt(res.data || 0);
+        $scope.pageCount = parseInt($attrs.ngpPageCount);
+        $scope.pages = new Array(Math.ceil($scope.total / $scope.pageCount));
+        if ($attrs.ngpStartPage) {
+          $scope.page = parseInt($attrs.ngpStartPage) || 1;
+          if ($scope.page > $scope.pages.length) {
+            $scope.page = $scope.pages.length;
+          }
+          if ($scope.page < 1) {
+            $scope.page = 1;
+          }
+          console.log("Start page: " + $scope.page);
+        }
+      }
+      function setPage(page) {
+        $scope.page = page;
+        $scope.pager(page);
+      }
+      function next() {
+        if ($scope.page < $scope.pages.length) {
+          setPage(++$scope.page);
+        }
+      }
+      function prev() {
+        if ($scope.page > 1) {
+          setPage(--$scope.page);
+        }
+      }
+      function reset() {
+        $http.get($attrs.ngpCountUrl).then(onCountSuccess);
+      }
+      $scope.setPage = setPage;
+      $scope.reset = reset;
+      $scope.next = next;
+      $scope.prev = prev;
+      $scope.page = 1;
+      reset();
+    }
+    function ngPaginationDirectiveTemplateUrlFn($element, $attrs) {
+      return $attrs.ngpTemplateUrl;
+    }
+    var ngPaginationDirectiveDef = {
+      templateUrl: ngPaginationDirectiveTemplateUrlFn,
+      link: ngPaginationDirectiveLinkFn,
+      restrict: "A",
+      scope: {
+        pager: "=ngpPager"
+      }
+    };
+    return ngPaginationDirectiveDef;
+  }
+  ng.module("ngPagination", []).directive("ngPagination", [ "$http", ngPaginationDirectiveFn ]);
+})(window);
